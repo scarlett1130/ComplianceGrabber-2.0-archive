37069787ccc975f13df95dce6f266631f8763249
Vatler Lett <business.xfree2000@gmail.com>
2022-10-25 17:46:30 -0700

    web scrapping by python deploy

--- Scrapper.py
+++ Scrapper.py
@@ -146,19 +146,22 @@ class Scrapper(Mouser):
             headers = {
                 'authority': 'www.te.com',
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-language': 'en-US,en;q=0.9',
+                'accept-language': 'en-US,en;q=0.9,de;q=0.8',
                 'cache-control': 'max-age=0',
+                'accept-encoding': 'gzip, deflate, br',
+                'scheme': 'https',
                 'content-type': 'application/x-www-form-urlencoded',
                 'referer': 'https://www.te.com/commerce/alt/product-compliance.do',
                 'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-                'sec-ch-ua-mobile': '?1',
-                'sec-ch-ua-platform': '"Android"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
                 'sec-fetch-dest': 'document',
                 'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'same-origin',
+                'sec-fetch-site': 'none',
                 'sec-fetch-user': '?1',
                 'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Mobile Safari/537.36'
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+
             }
 
             response = requests.request(
